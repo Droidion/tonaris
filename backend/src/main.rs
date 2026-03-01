9953138948cd8e7d0fea072c1d1d6f48932689e7
Denis Rodionov <nicholas.hawkwood@gmail.com>
2026-03-01 02:21:58 +0200

    add hello world api

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,6 @@
 use salvo::prelude::*;
 
-#[handler]
+#[endpoint]
 async fn hello() -> &'static str {
     "Hello World"
 }
@@ -8,8 +8,12 @@ async fn hello() -> &'static str {
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt().init();
+    let router = Router::new().push(Router::with_path("hello").get(hello));
+    let doc = OpenApi::new("test api", "0.0.1").merge_router(&router);
+    let router = router
+        .unshift(doc.into_router("/api-doc/openapi.json"))
+        .unshift(Scalar::new("/api-doc/openapi.json").into_router("/scalar"));
     let acceptor = TcpListener::new("0.0.0.0:8698").bind().await;
-    let router = Router::new().get(hello);
     println!("{router:?}");
     Server::new(acceptor).serve(router).await;
 }

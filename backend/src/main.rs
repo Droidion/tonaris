945dbd1db5677a5b7e1f246ca5c0b7e8350bcf73
Denis Rodionov <nicholas.hawkwood@gmail.com>
2026-03-01 02:11:26 +0200

    add salvo hello world

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use salvo::prelude::*;
+
+#[handler]
+async fn hello() -> &'static str {
+    "Hello World"
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt().init();
+    let acceptor = TcpListener::new("0.0.0.0:8698").bind().await;
+    let router = Router::new().get(hello);
+    println!("{router:?}");
+    Server::new(acceptor).serve(router).await;
 }

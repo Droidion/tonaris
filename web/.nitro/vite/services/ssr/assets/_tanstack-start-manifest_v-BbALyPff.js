cd9715deba0bd0ffefefac1ed77388cab1c2c82e
Denis Rodionov <nicholas.hawkwood@gmail.com>
2026-03-03 00:27:31 +0200

    del nitro

--- web/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-BbALyPff.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ "routes": { "__root__": { "filePath": "/Users/denis.rodionov/Projects/tonaris/web/src/routes/__root.tsx", "children": ["/"], "preloads": ["/assets/main-Eay3FFKB.js"], "assets": [] }, "/": { "filePath": "/Users/denis.rodionov/Projects/tonaris/web/src/routes/index.tsx", "assets": [], "preloads": ["/assets/index-DbTnBx2V.js"] } }, "clientEntry": "/assets/main-Eay3FFKB.js" });
-export {
-  tsrStartManifest
-};
